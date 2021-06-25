d6ea486312d2cd90aecd64860810efecb7cacdf7
sone <alftaketomo@gmail.com>
2021-06-25 07:26:47 +0000

    add debug (delve)

--- go/isuumo.sh
+++ go/isuumo.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 /home/isucon/isuumo/webapp/go/isuumo >> /home/isucon/isucon10-qualify-revenge/logs/app.log 2>&1
+
+# $ dlv exec /home/isucon/isuumo/webapp/go/isuumo --headless --log --listen=127.0.0.1:22345 --api-version=2 --accept-multiclient --continue
