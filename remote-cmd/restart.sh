6a5639b6e10744b8626e8fef20064bebcc1301a1
uzulla <uzulla@himitsukichi.com>
2021-06-25 13:16:21 +0900

    deploy script: remove Makefile

--- remote-cmd/restart.sh
+++ remote-cmd/restart.sh
@@ -1,6 +1,33 @@
 #!/bin/bash -eu
 
-source ~/.bashrc
+source "$HOME/.cargo/env"
+# BEGIN ANSIBLE MANAGED BLOCK Rust
+export PATH=/home/isucon/.cargo/bin:$PATH
+# END ANSIBLE MANAGED BLOCK Rust
+# BEGIN ANSIBLE MANAGED BLOCK deno
+export PATH=/home/isucon/.deno/bin:$PATH
+# END ANSIBLE MANAGED BLOCK deno
+# BEGIN ANSIBLE MANAGED BLOCK php
+export PATH=/home/isucon/local/php/bin:$PATH
+# END ANSIBLE MANAGED BLOCK php
+# BEGIN ANSIBLE MANAGED BLOCK Ruby
+export PATH=/home/isucon/local/ruby/bin:$PATH
+# END ANSIBLE MANAGED BLOCK Ruby
+# BEGIN ANSIBLE MANAGED BLOCK perl
+export PATH=/home/isucon/local/perl/bin:$PATH
+# END ANSIBLE MANAGED BLOCK perl
+# BEGIN ANSIBLE MANAGED BLOCK Node
+export PATH=/home/isucon/local/node/bin:$PATH
+# END ANSIBLE MANAGED BLOCK Node
+# BEGIN ANSIBLE MANAGED BLOCK go
+export PATH=/home/isucon/local/go/bin:/home/isucon/go/bin:$PATH
+export GOROOT=/home/isucon/local/go
+# END ANSIBLE MANAGED BLOCK go
+# BEGIN ANSIBLE MANAGED BLOCK python
+export PATH=/home/isucon/local/python/bin:$PATH
+# END ANSIBLE MANAGED BLOCK python
+
+### COPY
 
 cd /home/isucon/isucon10-qualify-revenge
 git pull
